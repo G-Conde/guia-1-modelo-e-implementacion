2f47ac8cb02754269878185c83044397a3157fb6
G-Conde <germanconde79@gmail.com>
2024-04-02 18:11:11 -0300

    Ejercicio 2

---
+++ Guia-1/src/Empleado.java
@@ -0,0 +1,66 @@
+/*2. Modele el objeto Empleado que posee las siguientes características, dni, nombre,
+apellido y salario. El mismo debe contar con la posibilidad de calcular el salario
+anual. A su vez se requiere otro método que permita aumentar el salario
+dependiendo del porcentaje que se le pase por parámetro. Considere crear un
+método que facilite imprimir por pantalla las características del objeto de la
+siguiente forma:
+Empleado[dni=?, nombre=?, apellido=?, salario=?]
+a. Inicialice un empleado Carlos Gutiérrez, con dni 23456345 y salario inicial de
+25000.
+b. Inicialice un empleado Ana Sánchez, con dni 34234123 y salario inicial de
+27500.
+c. Imprima ambos objetos por pantalla
+d. Aumente el salario del empleado Carlos en un 15% e imprima en pantalla el
+salario anual del mismo.
+*/
+public class Empleado {
+    private Integer dni;
+    private String name;
+    private String lastName;
+    private Double salary;
+
+    public Empleado() {
+    }
+
+    public Empleado(Integer dni, String name, String lastName, Double salary) {
+        this.dni = dni;
+        this.name = name;
+        this.lastName = lastName;
+        this.salary = salary;
+    }
+
+    public Integer getDni() {
+        return dni;
+    }
+
+    public void setDni(Integer dni) {
+        this.dni = dni;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public Double getSalary() {
+        return salary;
+    }
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+    public Double salaryAtYear() {
+        return salary * 12;
+    }
+    public void salaryIncrease(Double percent) {
+        this.salary = ((this.salary *percent)/100)+this.salary;
+    }
+    public void employe() {
+        System.out.println("[DNI=" + getDni()+", "+ "Empleado=" + getName()+", "+ "Apellido=" + getLastName()+", "+ "Salario=" + getSalary() + "]");
+    }
+}
--- Guia-1/src/Main.java
+++ Guia-1/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Rectangle rect= new Rectangle(1.0,1.0);
+        /*Rectangle rect= new Rectangle(1.0,1.0);
 
         System.out.println("Altura = " + rect.getHeight());
         System.out.println("Ancho = " + rect.getWidth());
@@ -13,5 +13,15 @@ public class Main {
 
         System.out.println("Perimetro =" + rect.perimeter());
         System.out.println("Area =" + rect.area());
+*/
+        Empleado empl1 = new Empleado(23456345,"Carlos","Gutierrez",25000.0);
+        Empleado empl2 = new Empleado(34234123,"Ana","Sanchez",27500.0);
+
+        empl1.employe();
+        empl2.employe();
+        empl1.salaryIncrease(15.0);
+        System.out.println("El salario con el aumento es " + empl1.getSalary());
+        System.out.println("El salario Anual de "+empl1.getName()+" es "+empl1.salaryAtYear());
+        empl1.employe();
     }
 }
